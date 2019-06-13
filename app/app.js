78257f88ac20508bf06b13cd6a4f05375cfce007
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-06-13 18:33:26 -0300

    Armazena do id dos players

--- app/app.js
+++ app/app.js
@@ -12,60 +12,45 @@ app.get('/', function(req, res){
     res.sendFile(path.resolve( 'public/index.html'));
 });
 
-let p1 = {
-    y: 20
-};
-let p2 = {
-    y: 20
-};
+let p1 = {};
+let p2 = {};
+
+function getPlayerById(id){
+    if(p1.id === id){
+        return p1;
+    }else if(p2.id === id){
+        return p2;
+    }else{
+        return null;
+    }
+}
+
 io.on('connection', function(socket){
     console.log('con');
 
     let id = socket.id;
 
-    if(p1.id){
-        p2.id = id;
-    }else {
-        p1.id = id;
+    if(!p1.id || !p2.id){
+        if(p1.id){
+            p2.id = id;
+        }else {
+            p1.id = id;
+        }
     }
 
     socket.emit('INIT_PARAMS', engine.getInitParams());
 
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        //TODO Remover player
     });
 
     socket.on('UP', function(msg){
-        console.log(socket.id);
-
-
-
-
-        console.log(p1);
-        console.log(p2);
-
     });
 
     socket.on('DOWN', function(msg){
-        if(id == p1.id){
-            p1.y -= 5;
-        }else{
-            p1.y -= 5;
-        }
-
-
-
-
-        console.log(p1);
-        console.log(p2);
-
     });
 });
-
-setTimeout(function () {
-    console.log('Emitindo');
-    io.emit('render', {p1: p1, p2: p2});
-}, 10000);
 
 http.listen(9001, function(){
     console.log('listening on *:9001');
